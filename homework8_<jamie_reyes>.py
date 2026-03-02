cdb25fda83e0231dbe3961165ebe163c42aa34ac
jxreyes <jxreyes@uw.edu>
2026-03-02 06:27:04 +0000

    week 8 homework

---
+++ homework8_<jamie_reyes>.py
@@ -0,0 +1,8 @@
+def sandwich(*ingredients):
+    print ("\nThis sandwich has: ")
+    for ingredients in ingredients:
+        print(ingredients)
+
+sandwich('sourdough','turkey', 'lettuce', 'tomatoes', 'mayo', 'mustard')
+sandwich('focaccia', 'steak', 'arugula', 'onions', 'aoli')
+
